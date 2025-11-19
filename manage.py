07671a05abd9f910ade2feb2b6e1f80d7ea1eb1a
Diego Aránguiz <diego.aranguiz10@inacapmail.cl>
2025-11-19 01:20:22 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ def main():
         sys.modules["cx_Oracle"] = oracledb
     except ImportError:
         pass
+        
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
